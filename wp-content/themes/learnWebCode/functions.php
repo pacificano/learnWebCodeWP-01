58325c42e12069043d875955b28e8be79cd644ae
pacificano <tris.pacificano@gmail.com>
2015-08-14 00:35:27 +1000

    learnWebCode

--- wp-content/themes/learnWebCode/functions.php
+++ wp-content/themes/learnWebCode/functions.php
@@ -14,3 +14,29 @@
 		'primary' => __('Primary Menu'),
 		'footer' => __('Footer Menu'),
 	));
+
+// Get top ancestor
+
+	function get_top_ancestor_id(){
+
+		global $post;
+
+		if ($post->post_parent) {
+			
+			$ancestors = array_reverse(get_post_ancestors($post->ID));
+			return $ancestors[0];
+
+		}
+
+		return $post -> ID;
+	}
+
+// Does page have children?
+
+	function has_children() {
+
+		global $post;
+		$pages = get_pages('child_of=' . $post->ID);
+		return count($pages);
+
+	}
--- wp-content/themes/learnWebCode/page.php
+++ wp-content/themes/learnWebCode/page.php
@@ -12,6 +12,31 @@
 
 
 	<article class="post page">
+
+		<?php if (has_children() OR $post->post_parent > 0) { ?>
+
+			<nav class="site-nav children-links clearfix">
+
+				<span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
+
+				<ul>
+
+					<?php  
+
+						$args = array(
+								'child_of' => get_top_ancestor_id(),
+								'title_li' => ''
+							);
+
+					?>
+
+					<?php wp_list_pages($args); ?>
+
+				</ul>
+
+			</nav>
+
+		<?php } ?>
 		
 		<h2><?php the_title(); ?></h2>
 	
