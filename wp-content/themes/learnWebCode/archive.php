3a373a6c5eda4c89e04b037220f3f3b12132d998
pacificano <tris.pacificano@gmail.com>
2015-08-15 14:25:17 +1000

    learnWebCode theme

--- wp-content/themes/learnWebCode/archive.php
+++ wp-content/themes/learnWebCode/archive.php
@@ -3,12 +3,54 @@
 	get_header();
 
 	if (have_posts()) :
+
+?>
+
+	<h2>
+		
+		<?php
+
+			if (is_category()) {
+
+				single_cat_title();
+
+			} elseif (is_tag()) {
+
+				single_tag_title();
+
+			} elseif (is_author()) {
+
+				the_post();
+				echo 'Author Archives: ' . get_the_author();
+				rewind_posts();
+
+			} elseif (is_day()) {
+
+				echo "Daily Archives: " . get_the_date();
+
+			} elseif (is_month()) {
+
+				echo "Monthly Archives: " . get_the_date('F Y');
+
+			} elseif (is_year()) {
+
+				echo "Yearly Archives: " . get_the_date('Y');
+
+			} else {
+
+				echo "Archives";
+
+			}
+
+		?>
+
+	</h2>
+
+<?php
 		
 	while (have_posts()) : the_post(); 
 
 ?>
-
-
 
 	<article class="post">
 		
@@ -41,7 +83,8 @@
 
 		</p>
 
-		<?php the_content(); ?>
+		<?php the_excerpt()(); ?>
+		<!-- changed the_content to the_excerpt. just shows a preview of the post, rather than the whole thing. -->
 
 	</article>
 		
