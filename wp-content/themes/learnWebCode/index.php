cff1393f35910c17ebc4bc02ac2ecd43fe737e36
pacificano <tris.pacificano@gmail.com>
2015-08-15 13:25:54 +1000

    learnWebCode theme

--- wp-content/themes/learnWebCode/index.php
+++ wp-content/themes/learnWebCode/index.php
@@ -14,6 +14,8 @@
 		
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	
+		<p class="post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></p>
+
 		<?php the_content(); ?>
 
 	</article>
