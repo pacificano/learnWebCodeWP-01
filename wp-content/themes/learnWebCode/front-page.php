a548f2c58baaa5c60b8177ef274a1a998ff4e301
pacificano <tris.pacificano@gmail.com>
2015-08-17 00:46:23 +1000

    learnWebCode theme

--- wp-content/themes/learnWebCode/front-page.php
+++ wp-content/themes/learnWebCode/front-page.php
@@ -4,34 +4,28 @@ get_header(); ?>
 
 	<!-- site-content -->
 	<div class="site-content clearfix">
+
+		<h3>Custom HTML Here!</h3>
 		
-		<!-- main-column -->
-		<div class="main-column">
-			
-			<?php
-			
+		<?php  
+
 			if (have_posts()) :
+				
 				while (have_posts()) : the_post();
 
-				if (get_post_format() == false) {
-					get_template_part('content', 'single');
-				} else {
-					get_template_part('content', get_post_format());
-				}
-
+				the_content(); 
 
 				endwhile;
 
 				else :
+
 					echo '<p>No content found</p>';
 
 				endif;
 			
-			?>
-			
-		</div><!-- /main-column -->
-		
-		<?php get_sidebar(); ?>
+		?>
+
+		<h1>BOOOO! ~ghost time!</h1>
 		
 	</div><!-- /site-content -->
 
